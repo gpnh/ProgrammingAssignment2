0366bb7218a6c9b30c89325346f450b4e308d639
gpnh <gpnh@iol.ie>
2014-06-21 18:23:44 +0100

    Program updated to invert a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## These functions return the inverse of a matrix using the function solve.
+## If the inverse matrix is already held in cache, then this is used
+## in order to avoid the need to repeat the inversion calcualtion.
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## The first function receives a matrix as input.
+## NB:The input must be a square matrix which is invertible
+  
+      m <- NULL
+      set <- function(y) {
+      x <<- y
+      m <<- NULL
+      }
+  
+##  The function then return a list of the four functions used to
+##  process, get and store the matrix in the local environment
+  
+      get <- function() x
+      setmatrix <- function(mnew) m <<- mnew
+      getmatrix <- function() m
+      list(set = set, get = get,
+      setmatrix = setmatrix,
+      getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function returns a matrix that is the inverse of 'x'
+  
+          m <- x$getmatrix()
+          
+##  If the matrix is already held in cache
+##  and so has not been set to NULL, the cached
+##  version is returned
+
+          if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+          }        
+
+##   If the matrix is not held in cache then the inverse is calculated
+##   using solve and this is returned.  The cache is updated with this
+##   new matrix.
+          d <- x$get()
+          m <- solve(d, ...)
+          x$setmatrix(m)
+          m
 }
+
+
+
